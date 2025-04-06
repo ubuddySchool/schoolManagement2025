875418c866bce111375159f7faddedfe87b0e89a
ubuddySchool <ubuddyhosting377@gmail.com>
2025-04-06 17:27:56 +0530

    in admin school list and sidebar and new school add

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -16,14 +16,18 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/icons/flags/flags.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/datatables/datatables.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+
 </head>
 
 <body class="mini-sidebar">
--- resources/views/admin/school_admin/create.blade.php
+++ resources/views/admin/school_admin/create.blade.php
@@ -1,38 +1,164 @@
-<!-- resources/views/admin/school_admin/create.blade.php -->
-
 @extends('admin.layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Create School Admin</h1>
+<div class="content container-fluid">
 
-        <form method="POST" action="{{ route('school-admin.store') }}">
-            @csrf
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" required>
-                @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
 
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}" required>
-                @error('email')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+    <!-- Add School Form -->
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Add School</h4>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('school-admin.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="row g-3">
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">School Name <span class="login-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Enter School Name" name="name" id="name" value="{{ old('name') }}">
+                            @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
 
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" name="password" id="password" required>
-                @error('password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">School Code <span class="login-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Enter School Code" name="school_code" required>
+                        </div>
+                    </div>
 
-            <button type="submit" class="btn btn-primary">Create Admin</button>
-        </form>
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Dice Code <span class="login-danger">*</span></label>
+                            <input type="text" class="form-control" placeholder="Enter Dice Code" name="dice_code" required>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Board Name <span class="login-danger">*</span></label>
+                            <select class="form-select" name="board_name" required>
+                                <option selected disabled>Select Board</option>
+                                <option value="CBSE">CBSE</option>
+                                <option value="ICSE">ICSE</option>
+                                <option value="State">State</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Medium <span class="login-danger">*</span></label>
+                            <select class="form-select" name="medium" required>
+                                <option selected disabled>Please Select Medium</option>
+                                <option value="English">English</option>
+                                <option value="Hindi">Hindi</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Number of Students</label>
+                            <input type="number" class="form-control" placeholder="Enter Number of students" name="students">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Contact Number</label>
+                            <input type="text" class="form-control" placeholder="Enter Contact Number" name="contact">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">School Email ID <span class="login-danger">*</span></label>
+                            <input type="email" class="form-control" placeholder="Enter Email Address" name="email" id="email" value="{{ old('email') }}" >
+                            @error('email')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Address</label>
+                            <input type="text" class="form-control" placeholder="Enter School Address" name="address">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">City</label>
+                            <input type="text" class="form-control" placeholder="Enter City" name="city">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">State</label>
+                            <input type="text" class="form-control" placeholder="Enter State" name="state">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Pin Code</label>
+                            <input type="text" class="form-control" placeholder="Enter Pin Code" name="pin">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Locality</label>
+                            <input type="text" class="form-control" placeholder="Enter Locality" name="locality">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Website</label>
+                            <input type="url" class="form-control" placeholder="Enter Website Link" name="website">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Username</label>
+                            <input type="text" class="form-control" value="ubuddy" >
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password" id="password">
+                            <div id="password-strength" class="form-text text-success">Your password is strong</div>
+                            @error('password')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group local-forms">
+                            <label class="form-label">Upload Image</label>
+                            <input type="file" class="form-control" name="image">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-4">
+                    <button type="submit" class="btn btn-primary">Add School</button>
+                </div>
+            </form>
+
+        </div>
     </div>
+
+</div>
 @endsection
