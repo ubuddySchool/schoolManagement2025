e97c439e0d9f9ac426fb2540c094867a16c836f7
ubuddySchool <ubuddyhosting377@gmail.com>
2025-04-18 14:11:30 +0530

    result marks range has been done

--- resources/views/school/result/view_marksheet.blade.php
+++ resources/views/school/result/view_marksheet.blade.php
@@ -12,12 +12,12 @@
                 <div class="card">
                     <div class="card-header">
                         <!-- <h4 class="card-title mb-0">Subject - English</h4> -->
-                        <h6 class=" card-title m-0 w-100">2025-25 |  Term I  |  Class VI  | Section A</h6>
+                        <h6 class=" card-title m-0 w-100">2025 | Term I | Class VI | Section A</h6>
                     </div>
                     <div class="card-body p-4">
                         <table class="table table-bordered">
                             <tbody>
-                                
+
                                 <tr>
                                     <th>Serial No.</th>
                                     <th>Roll No.</th>
@@ -32,6 +32,35 @@
                                     <td>Akash Singh</td>
                                     <td><input type="number" class="form-control marks" placeholder="Enter Marks"></td>
                                     <td><input type="number" class="form-control marks" placeholder="Enter Marks"></td>
+                                    <td>
+                                        <input type="checkbox" class="btn btn-warning absent-btn" name="" id="absent-checkbox">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>23456</td>
+                                    <td>Sunil Singh</td>
+                                    <td>
+                                        <label><input type="radio" name="grade" value="A1"> A1</label>
+                                        <label><input type="radio" name="grade" value="A2"> A2</label>
+                                        <label><input type="radio" name="grade" value="B1"> B1</label>
+                                        <label><input type="radio" name="grade" value="B2"> B2</label>
+                                        <label><input type="radio" name="grade" value="C1"> C1</label>
+                                        <label><input type="radio" name="grade" value="C2"> C2</label>
+                                        <label><input type="radio" name="grade" value="D"> D</label>
+                                        <label><input type="radio" name="grade" value="E"> E</label>
+                                    </td>
+                                    <td>
+                                        <label><input type="radio" name="grade" value="A1"> A1</label>
+                                        <label><input type="radio" name="grade" value="A2"> A2</label>
+                                        <label><input type="radio" name="grade" value="B1"> B1</label>
+                                        <label><input type="radio" name="grade" value="B2"> B2</label>
+                                        <label><input type="radio" name="grade" value="C1"> C1</label>
+                                        <label><input type="radio" name="grade" value="C2"> C2</label>
+                                        <label><input type="radio" name="grade" value="D"> D</label>
+                                        <label><input type="radio" name="grade" value="E"> E</label>
+                                    </td>
+
                                     <td>
                                         <input type="checkbox" class="btn btn-warning absent-btn" name="" id="absent-checkbox">
                                     </td>
@@ -60,10 +89,10 @@
         // Initially disable the input fields when the page loads
         checkbox.change(function() {
             if (this.checked) {
-                marksInputs.prop('disabled', true);  
+                marksInputs.prop('disabled', true);
             } else {
                 // Enable input fields if checkbox is unchecked
-                marksInputs.prop('disabled', false); 
+                marksInputs.prop('disabled', false);
             }
         });
 
