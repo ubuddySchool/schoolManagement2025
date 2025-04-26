9e50b5962159e387e54c5c71a23ca39d9ce2174b
amit dungarwal <amit.dungarwal@oyelabs.com>
2025-04-26 13:52:29 +0530

    master changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,11 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(AdminSeeder::class); 
-        $this->call(MasterModuleSeeder::class); 
-        $this->call(MasterClassesSeeder::class); 
-        $this->call(MasterSubjectSeeder::class); 
-        $this->call(MasterTermsSeeder::class); 
-
+        
         User::factory()->create([
             'name' => 'schoolududdy@123',
             'school_username' => 'schoolududdy123',
