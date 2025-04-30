20be40badedab668208f384c84e825785fbc57ec
amit dungarwal <amit.dungarwal@oyelabs.com>
2025-04-30 12:27:03 +0530

    master classes

--- resources/views/admin/basic_configuration/class.blade.php
+++ resources/views/admin/basic_configuration/class.blade.php
@@ -20,32 +20,43 @@
                         </div>
                     </div>
 
-                    <?php
-                    $labels = ['Play Nursery', 'Nursery', 'K.G. I', 'K.G. II', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI PCM', 'XI PCB', 'XI Commerce', 'XI ARTS', 'XI EXTRA', 'XII PCM', 'XII PCB', 'XII Commerce', 'XI ARTS', 'XII EXTRA'];
-                    ?>
+                    <form action="#" method="POST">
+                        @csrf
+                        @foreach ($classes as $index => $label)
+                            <div class="row mb-1">
+                                <div class="col-md-3 col-sm-3">
+                                    <div class="form-check d-flex align-items-center gap-2">
+                                        <input 
+                                            class="form-check-input m-0" 
+                                            type="checkbox" 
+                                            id="checkbox{{ $index }}" 
+                                            name="selectedClasses[]" 
+                                            value="{{ $index }}"
+                                            {{ in_array($index, old('selectedClasses', [])) ? 'checked' : '' }}
+                                        >
+                                        <label class="form-check-label mb-0" for="checkbox{{ $index }}">
+                                            {{ $label->class_name }}
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-3">
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        name="reClassName{{ $index }}" 
+                                        placeholder="Enter New Name of Class" 
+                                        value="{{ old('reClassName' . $index) }}"
+                                    >
+                                </div>
+                            </div>
+                        @endforeach
 
-                    <?php foreach ($labels as $index => $label): ?>
-                    <div class="row mb-1">
-                        <div class="col-md-2 col-sm-3">
-                            <div class="form-check d-flex align-items-center gap-2">
-                                <input class="form-check-input m-0" type="checkbox" id="checkbox<?= $index ?>">
-                                <label class="form-check-label mb-0" for="checkbox<?= $index ?>">
-                                    <?= $label ?>
-                                </label>
+                        <div class="row">
+                            <div class="col-12 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary" name="submitClass">Submit</button>
                             </div>
                         </div>
-                        <div class="col-md-3 col-sm-3">
-                            <div class="form-check">
-                                <input type="text" class="form-control" name="reClassName<?= $index ?>" placeholder="Enter New Name of Class">
-                            </div>
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                    <div class="row">
-                        <div class="col-12 d-flex justify-content-center">
-                            <button type="submit" class="btn btn-primary" name="submitClass">Submit</button>
-                        </div>
-                    </div>
+                    </form>
                     
                 </div>
             </div>
